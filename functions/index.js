591ec722dfb58f4b75c3793417518bc37c8c4b55
Vivek Chaudhary <vkc369258@gmail.com>
2022-10-30 13:35:16 +0000

    twitter oauth2.0 success

--- functions/index.js
+++ functions/index.js
@@ -6,24 +6,28 @@ admin.initializeApp();
 // Database Reference
 const dbRef = admin.firestore().doc('tw/tokens');
 
+// Status check for serverless functions
+exports.checkStatus = functions.https.onRequest((request, response) => {
+  functions.logger.info('Check Status!', { structuredData: true });
+  response.json({
+    success: true,
+    author: process.env.AUTHOR,
+    msg: 'Serverless functions online!',
+  });
+});
+
 // Twitter API Init
 const twitterApi = require('twitter-api-v2').default;
-console.log(process.env.TW_CLIENT_ID, process.env.TW_CLIENT_SECRET);
 const twitterClient = new twitterApi({
   clientId: process.env.TW_CLIENT_ID,
   clientSecret: process.env.TW_CLIENT_SECRET,
 });
 
 // Callback URL
-const callBackURL = process.env.CB_GITPOD_URL;
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+const callBackURL = process.env.CB_URL;
 
 // STEP 1 - Auth URL
-exports.auth = functions.https.onRequest(async (req, res) => {
+exports.auth = functions.https.onRequest(async (request, response) => {
   const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
     callBackURL,
     {
@@ -33,13 +37,11 @@ exports.auth = functions.https.onRequest(async (req, res) => {
 
   // Store verifier
   await dbRef.set({ codeVerifier, state });
-  response.send('Hello from Firebase! ${url} \n ${codeVerifier} \n ${state}');
-  // response.redirect(url);
+  response.redirect(url);
 });
 
-exports.callback = functions.https.onRequest((req, res) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+// STEP 2 - Verify callback code, store access_token
+exports.callback = functions.https.onRequest((request, response) => {});
 
-exports.tweet = functions.https.onRequest((req, res) => {});
+// STEP 3 - Refresh tokens and post tweets
+exports.tweet = functions.https.onRequest((request, response) => {});
