71c0995508a5d570d1d9932cb9d6f2527a73ab4f
Vivek Chaudhary <vkc369258@gmail.com>
2022-11-01 15:03:46 +0000

    Minor changes in checkOpenAI.js

--- functions/utils/checkOpenAI.js
+++ functions/utils/checkOpenAI.js
@@ -1,10 +1,14 @@
 const { createTweet } = require('../openai');
+const { generatePrompt } = require('./createPrompt');
 
 // NOTE: This is a function to test the Open AI endpoint w/o running Firebase.
 const checkAPI = async () => {
   const newTweet = (await createTweet()).data.choices[0].text;
   console.log(newTweet);
 };
+
+// To check random unique prompt
+console.log(generatePrompt());
 
 // Call this function when you are ready. Each execution to Open AI is chargeable.
 // checkAPI();
